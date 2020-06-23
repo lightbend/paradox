6ea48b95f7a4856fd83d8e9ff451935f59080b5d
Scala Steward <me@scala-steward.org>
2020-06-23 22:38:33 +0200

    Update ST4 to 4.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Version {
   val prettify   = "4-Mar-2013-1"
   val sbtWeb     = "1.4.4"
   val scalatest  = "3.2.0"
-  val st4        = "4.3"
+  val st4        = "4.3.1"
   val jsoup      = "1.13.1"
 }
 
