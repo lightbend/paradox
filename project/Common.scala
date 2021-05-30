ab1952600f857c0df589ef7b42801978c2a097fd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-31 01:07:13 +0200

    Update scala-library to 2.12.14 (#489)

--- project/Common.scala
+++ project/Common.scala
@@ -32,7 +32,7 @@ object Common extends AutoPlugin {
 
   // AutomateHeaderPlugin is not an allRequirements-AutoPlugin, so explicitly add settings here:
   override def projectSettings = AutomateHeaderPlugin.projectSettings ++ Seq(
-    scalaVersion := "2.12.13",
+    scalaVersion := "2.12.14",
     scalacOptions ++= Seq("-encoding", "UTF-8", "-unchecked", "-deprecation", "-feature"),
     javacOptions ++= Seq("-encoding", "UTF-8"),
     resolvers += Resolver.typesafeIvyRepo("releases"),
