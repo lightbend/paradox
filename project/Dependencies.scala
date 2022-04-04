49b516cdf9b1c6310b55594dfa1d96a73f33d167
Scala Steward <me@scala-steward.org>
2022-04-04 02:03:38 +0200

    Revert commit(s) 5b8edbe

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Version {
   val parboiled  = "1.3.1"
   val prettify   = "4-Mar-2013-1"
   val sbtWeb     = "1.4.4"
-  val scalatest  = "3.2.11"
+  val scalatest  = "3.2.9"
   val st4        = "4.3.1"
   val jsoup      = "1.14.2"
 }
