ef2f68a96042385db55be998295e6f40a2fce924
Scala Steward <scala_steward@virtuslab.com>
2023-01-07 20:18:20 +0000

    Update scalatest to 3.2.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Version {
   val parboiled  = "1.4.1"
   val prettify   = "4-Mar-2013-1"
   val sbtWeb     = "1.4.4"
-  val scalatest  = "3.2.14"
+  val scalatest  = "3.2.15"
   val st4        = "4.3.4"
   val jsoup      = "1.15.3"
 }
