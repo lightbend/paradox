4dda02b6ef48de31e0178da283e2343b32a7ab82
Scala Steward <scala_steward@virtuslab.com>
2024-02-19 23:21:36 +0000

    bump: scalatest 3.2.18 (was 3.2.17)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Version {
   val parboiled  = "1.4.1"
   val prettify   = "4-Mar-2013-1"
   val sbtWeb     = "1.5.3"
-  val scalatest  = "3.2.17"
+  val scalatest  = "3.2.18"
   val st4        = "4.3.4"
   val jsoup      = "1.16.1"
 }
